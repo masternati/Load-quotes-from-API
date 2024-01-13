dbeba7387d131454b8491f40383d62d49814b93c
masternati <sohrabhossain207@gmail.com>
2024-01-13 23:35:01 +0600

    load  rest country api and display 250 countries information as like country name , capital and others information

--- Load-quotes-from-API/js/js/countryapi.js
+++ Load-quotes-from-API/js/js/countryapi.js
@@ -9,6 +9,7 @@ const displayCountry = countries =>{
 
     countries.forEach(country => {
         const createDiv = document.createElement('div');
+        createDiv.classList.add('country');
 
 
         createDiv.innerHTML= `
@@ -16,8 +17,27 @@ const displayCountry = countries =>{
 
             <!--if all the values are not in the list  -->
             <p>Country Capital: ${country.capital? country.capital[0] :'no capital'}</p>
+
+            <button onclick="countryDetails('${country.cca2}')">Details</button>
         `
 
         countryContainer.appendChild(createDiv);
     });
 }
+
+const countryDetails = (code) =>{
+    //https://restcountries.com/v3.1/alpha/
+    const countryApiUrl = `https://restcountries.com/v3.1/alpha/${code}`
+    // console.log('clicked country details',code);
+    // console.log(countryApiUrl);
+
+    fetch(countryApiUrl)
+    .then(res => res.json())
+    .then(data => displayCountryDetails(data))
+}
+
+const displayCountryDetails = country =>{
+    console.log(country);
+}
+
+countryApi();
