0e3bccc9cabb980e3cfa9715e2002b522488bc45
masternati <sohrabhossain207@gmail.com>
2024-01-13 11:08:52 +0600

    load quotes from api

---
+++ module-33/js/jsonApi.js
@@ -0,0 +1,17 @@
+const loadJsonApi = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => displayJsonApi(data));
+};
+
+const displayJsonApi = (jsonApis) => {
+    const jsonData = document.getElementById('jsonApi');
+    for (const jsonApi of jsonApis) {
+        const createOrderList = document.createElement('ol');
+        createOrderList.innerText = jsonApi.email;
+        jsonData.appendChild(createOrderList);
+    }
+};
+
+// Call the loadJsonApi function
+// loadJsonApi();
--- module-33/js/restAPI.js
+++ module-33/js/restAPI.js
@@ -1,5 +1,11 @@
 const loadApi = ()=>{
     fetch ('https://api.kanye.rest/')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayQoute(data))
+}
+
+const displayQoute = api =>{
+    const blockQuote = document.getElementById('api');
+    console.log(api);
+    blockQuote.innerText = api.quote;
 }
