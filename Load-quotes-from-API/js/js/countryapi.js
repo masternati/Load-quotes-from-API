1373711d8f1e474680088ba31b54ab761581388b
masternati <sohrabhossain207@gmail.com>
2024-01-14 09:39:32 +0600

    dispaly country name country code country flag from the api

--- Load-quotes-from-API/js/js/countryapi.js
+++ Load-quotes-from-API/js/js/countryapi.js
@@ -29,15 +29,23 @@ const countryDetails = (code) =>{
     //https://restcountries.com/v3.1/alpha/
     const countryApiUrl = `https://restcountries.com/v3.1/alpha/${code}`
     // console.log('clicked country details',code);
-    // console.log(countryApiUrl);
+    console.log(countryApiUrl);
 
     fetch(countryApiUrl)
     .then(res => res.json())
-    .then(data => displayCountryDetails(data))
+    .then(data => displayCountryDetails(data[0]))
 }
 
 const displayCountryDetails = country =>{
-    console.log(country);
+    const displayCountry = document.getElementById('display-country');
+    displayCountry.innerHTML = `
+        <h2>Country Name: ${country.name.common}</h2>
+        <h2>Country Code: ${country.cca2}</h2>
+        <p>Country Flag- </p>
+        <img src="${country.flags.png}" alt="">
+    `
+
+
 }
 
 countryApi();
