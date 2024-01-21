24ee2066a4390bdc346431cb8d507147e9db2775
masternati <sohrabhossain207@gmail.com>
2024-01-21 11:19:40 +0600

    search food name, foods will be displayed on the website from API

--- Load-quotes-from-API/js/js/mealbd.js
+++ Load-quotes-from-API/js/js/mealbd.js
@@ -6,16 +6,17 @@ const loadMEals = (search)=>{
 };
 
 const displayMeals = meals =>{
+    // console.log(meals);
+    
     const mealContainer = document.getElementById('meal-container');
-
     mealContainer.innerHTML = '';
 
     meals.forEach(meal => {
         const mealDiv = document.createElement('div');
-        console.log(meal);
+
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
-        <div class="card">
+        <div onclick =" loadMealDetails(${meal.idMeal})" class="card">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -34,6 +35,33 @@ const searchFood = ()=>{
     loadMEals(searchText);
     searchField.value = '';
 }
+const loadMealDetails = (idMeal)=>{
+    // console.log("meal id", idMeal);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
 
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetails(data.meals[0]))
+}
 
-// loadMEals();
+const displayMealDetails = meal=>{
+    const displayMealContainer = document.getElementById('display-meal-container');
+    displayMealContainer.innerHTML = '';
+
+    const createDiv = document.createElement('div');
+    createDiv.classList.add('card');
+    createDiv.innerHTML = `
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">Meal-Id: ${meal.idMeal}</p>
+            <p class="card-text">Meal-Id: ${meal.idMeal}</p>
+            <p class="card-text">Meal-Category: ${meal.strCategory}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    
+    `;
+    displayMealContainer.appendChild(createDiv);
+}
+
+loadMEals('a');
